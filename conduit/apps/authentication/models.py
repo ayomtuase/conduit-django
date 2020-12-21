33aaa39e00684a8dda97520337f129b6e50fbee3
Ayomide Oguntuase <oguntuase3@gmail.com>
2020-12-21 19:13:59 +0100

    Made migrations, debugged models.py

--- conduit/apps/authentication/models.py
+++ conduit/apps/authentication/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 from django.contrib.auth.models import (
-		AbstractBaseUser, BaseUserManager, PermissionsMixins
+		AbstractBaseUser, BaseUserManager, PermissionsMixin
 	) 
 
 import jwt
@@ -35,12 +35,12 @@ class UserManager(BaseUserManager):
 
 		return user
 
-class User(AbstractBaseUser, PermissionsMixins):
+class User(AbstractBaseUser, PermissionsMixin):
 	username = models.CharField(db_index=True, max_length=200, unique=True)
 	email = models.EmailField(db_index=True, unique=True)
 	is_active = models.BooleanField(default=True)
 	is_staff = models.BooleanField(default=False)
-	created_at = models.DateTimeField(auto_add_now=True)
+	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
 
 	USERNAME_FIELD = 'email' 
