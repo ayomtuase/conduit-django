9937d4ca750c7ad40636b0ef55bd3376433c9eff
Ayomide Oguntuase <oguntuase3@gmail.com>
2021-01-05 01:09:58 +0100

    Update urls.py

--- conduit/urls.py
+++ conduit/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api', include('apps.authentication.urls'))
+    path('api/', include('conduit.apps.authentication.urls', namespace='authentication'))
 ]
