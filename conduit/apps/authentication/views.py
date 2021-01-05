70b80a045aa140fa4cb97c47548d7242feba88c0
Ayomide Oguntuase <oguntuase3@gmail.com>
2021-01-05 01:10:36 +0100

    Update views.py

--- conduit/apps/authentication/views.py
+++ conduit/apps/authentication/views.py
@@ -3,10 +3,12 @@ from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from .serializers import RegistrationSerializer
+from .renderers import UserJSONRenderer
 
 
-class RegistrationView(APIView):
-	permissions = (AllowAny,)
+class RegistrationAPIView(APIView):
+	permission_classes = (AllowAny,)
+	renderer_classes = (UserJSONRenderer,)
 	serializer_class = RegistrationSerializer
 
 	def post(self, request):
