98370a0e36b03c9cef8d15c87fc44e2d1bd92a08
Ayomide Oguntuase <oguntuase3@gmail.com>
2021-01-07 09:31:58 +0100

    Update urls.py

--- conduit/apps/authentication/urls.py
+++ conduit/apps/authentication/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-
-from .views import RegistrationAPIView
+from .views import RegistrationAPIView, LoginAPIView
 
 app_name = 'authentication'
 urlpatterns = [
-		path('users', RegistrationAPIView.as_view(), name='authentication'),	
+		path('users/', RegistrationAPIView.as_view(), name='authentication'),
+		path('users/login/', LoginAPIView.as_view())	
 	]
