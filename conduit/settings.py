286fb49b61864f98a0f9d0d3880a682aaaf8643f
Ayomide Oguntuase <oguntuase3@gmail.com>
2021-01-07 09:34:40 +0100

    Override DRF settings for exception

--- conduit/settings.py
+++ conduit/settings.py
@@ -128,3 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'EXCEPTION_HANDLER' : 'conduit.apps.core.exceptions.core_exception_handler',
+    'NON_FIELD_ERRORS_KEY' : 'error' 
+}
