019a8545fe0765caf92856edbfdc4c5b05c2937d
Ayomide Oguntuase <oguntuase3@gmail.com>
2021-01-11 22:57:27 +0100

    Update renderers.py
    Allow DRF handle errors in the response so as to have an 'errors' namespace

--- conduit/apps/authentication/renderers.py
+++ conduit/apps/authentication/renderers.py
@@ -5,9 +5,14 @@ class  UserJSONRenderer(JSONRenderer):
 	charset = 'utf-8'
 
 	def render(self, data, media_type=None, renderer_context=None):
+
+		errors = data.get('errors', None)		
 		
 		token = data.get('token', None)
 		if token is not None and isinstance(token, bytes):
 			data['token'] = token.decode(charset)
+
+		if errors is not None:
+			return super(UserJSONRenderer, self).render(data)
 
 		return json.dumps({'user' : data})
